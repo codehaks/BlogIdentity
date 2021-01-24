1835ef78ba1def52fd204258ddad59002018f85d
Hakim Ghods <ghods.hakim@hotmail.com>
2021-01-24 10:35:10 +0330

    fix

--- BlogIdentity/Pages/Details.cshtml.cs
+++ BlogIdentity/Pages/Details.cshtml.cs
@@ -1,16 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using BlogIdentity.Common;
 using BlogIdentity.Data;
 using BlogIdentity.Models;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BlogIdentity.Pages
 {
-    [TypeFilter(typeof(HitCounterAttribute))]
     public class DetailsModel : PageModel
     {
         private readonly ApplicationDbContext _db;
@@ -24,7 +17,6 @@ namespace BlogIdentity.Pages
 
         public void OnGet(int id)
         {
-            HttpContext.Items.Add("id", id);
             Post = _db.Posts.Find(id);
         }
     }
